bd9a3c43eaf47c4cc10e81e00e944c82c7908de9
Kateryna <kat.rin47u@gmail.com>
2024-10-11 10:35:04 +0100

    feat: add CareTaker model

--- shelters/models.py
+++ shelters/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
@@ -13,3 +14,12 @@ class Shelter(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class CareTaker(AbstractUser):
+    phone_number = models.CharField(max_length=15)
+    address = models.CharField(max_length=255)
+    years_of_experience = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
