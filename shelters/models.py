19d33a6b75bf6d3e4bd3283c93f28cfbd45fb1d0
Kateryna <kat.rin47u@gmail.com>
2024-10-11 10:20:59 +0100

    feat: add Shelter model

--- shelters/models.py
+++ shelters/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Shelter(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    location = models.CharField(max_length=255)
+    contact_number = models.CharField(max_length=15)
+    capacity = models.IntegerField()
+    current_animal_count = models.IntegerField()
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
