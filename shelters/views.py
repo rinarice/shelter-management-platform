51e3f081a1b45aa0a47716312db3ede57adf9a7c
Kateryna <kat.rin47u@gmail.com>
2024-10-16 00:02:41 +0100

    feat: add index view with shelter, animal, and caretaker counts

--- shelters/views.py
+++ shelters/views.py
@@ -1,3 +1,18 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from shelters.models import Shelter, Animal, CareTaker
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    num_shelters = Shelter.objects.count()
+    num_animals = Animal.objects.count()
+    num_caretakers = CareTaker.objects.count()
+
+    context = {
+        "num_shelters": num_shelters,
+        "num_animals": num_animals,
+        "num_caretakers": num_caretakers,
+    }
+
+    return render(request, "shelters/index.html", context)
