9600365388f9c02eb6593188a65ed60b40c6720f
Kateryna <kat.rin47u@gmail.com>
2024-10-16 10:29:59 +0100

    feat: add URL for AnimalListView

--- shelters/urls.py
+++ shelters/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from shelters.views import index, ShelterListView
+from shelters.views import (
+    index,
+    ShelterListView,
+    AnimalListView,
+)
 
 app_name = "shelters"
 
 urlpatterns = [
     path("", index, name="index"),
     path("shelters/", ShelterListView.as_view(), name="shelter_list"),
+    path("animals/", AnimalListView.as_view(), name="animal_list"),
 ]
