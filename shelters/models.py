a25a81e12f923f844d7a3e86082515fa1c552378
Kateryna <kat.rin47u@gmail.com>
2024-11-06 11:16:48 +0000

    chore(models): add ordering by username to CareTaker model

--- shelters/models.py
+++ shelters/models.py
@@ -21,6 +21,9 @@ class CareTaker(AbstractUser):
     address = models.CharField(max_length=255)
     years_of_experience = models.IntegerField(null=True, blank=True)
 
+    class Meta:
+        ordering = ["username"]
+
     def __str__(self):
         return f"{self.username} ({self.first_name} {self.last_name})"
 
