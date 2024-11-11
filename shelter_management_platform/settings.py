bb6233d34430a2f00221453ee3135579f762eb3c
Kateryna <kat.rin47u@gmail.com>
2024-11-11 14:02:24 +0000

    chore: add .env.sample and update settings for secret key management
    - Add .env.sample as a template for environment variables
    - Update settings.py to load SECRET_KEY from .env using python-dotenv
    - Modify .gitignore to exclude .env file from version control

--- shelter_management_platform/settings.py
+++ shelter_management_platform/settings.py
@@ -20,7 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-(6_mt0z4%rnoc0i4$^)ldpj(oojg(t(^dz!aw(zua04wrw09ma"
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
